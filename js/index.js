577a7e83951832bcce5402835f58127f898fc963
Tanzir Rahman <tanzir71@gmail.com>
2018-01-18 17:39:16 +0600

    Updates.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ var textOffice = $('#textOffice');
 var textEmail = $('#textEmail');
 var textPhone = $('#textPhone');
 var textWeb = $('#textWeb');
+var textFB = $('#textFB');
+var textTweet = $('#textTweet');
+var textLinkedIn = $('#textLinkedIn');
+var textOtherWeb = $('#textOtherWeb');
 
 $('#nameInput').on('input',function(e){
   var nameInput = $('#nameInput').val();
@@ -35,4 +39,50 @@ $('#webInput').on('input',function(e){
   var webInput = $('#webInput').val();
   textWeb.text(webInput);
   textWeb.attr("href", "http://"+webInput);
+  webInput.includes("http") && swal("Don't include HTTP.","Select it from the drop-down.", "warning");
 });
+
+$('#secureWeb').change(function(e){
+  var webInput = $('#webInput').val();
+  var secureWeb = $('#secureWeb').val();
+  textWeb.attr("href", secureWeb+webInput);
+});
+
+$('#fbInput').on('input',function(e){
+  var fbInput = $('#fbInput').val();
+  textFB.attr("href", fbInput);
+  if(fbInput.length > 10 && fbInput.includes("http") !== true) { swal("Include HTTPS.","Start your URL with https://", "warning"); }
+});
+
+$('#tweetInput').on('input',function(e){
+  var tweetInput = $('#tweetInput').val();
+  textTweet.attr("href", tweetInput);
+  if(tweetInput.length > 10 && tweetInput.includes("http") !== true) { swal("Include HTTPS.","Start your URL with https://", "warning"); }
+});
+
+$('#linkedinInput').on('input',function(e){
+  var linkedinInput = $('#linkedinInput').val();
+  textLinkedIn.attr("href", linkedinInput);
+  if(linkedinInput.length > 10 && linkedinInput.includes("http") !== true) { swal("Include HTTPS.","Start your URL with https://", "warning"); }
+});
+
+$('#otherInput').on('input',function(e){
+  var otherInput = $('#otherInput').val();
+  textOtherWeb.attr("href", otherInput);
+  if(otherInput.length > 10 && otherInput.includes("http") !== true) { swal("Include HTTP.","Start your URL with http://", "warning"); }
+});
+
+function CopyToClipboard(containerid) {
+if (document.selection) { 
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand("copy"); 
+
+} else if (window.getSelection) {
+    var range = document.createRange();
+     range.selectNode(document.getElementById(containerid));
+     window.getSelection().addRange(range);
+     document.execCommand("copy");
+     swal("Signature copied!","Successfully copied to clipboard.", "success");
+}}
